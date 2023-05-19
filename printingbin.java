5f975411110332f918862915369112e5a7c17c8d
Subroto Banerjee <subroto.2003@gmail.com>
2023-05-19 06:38:51 +0530

    Add printing binary arrays code

---
+++ printingbin.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+    public class printingbin {
+    	public static void convertArray(int newarr[], int arr[], int n){
+	        for( int i = 0 ; i<n ; i++){
+	            newarr[i] = arr[i]^1;
+	        }
+	    }
+	
+	public static void printArray(int arr[]){
+	    for(int i=0 ; i<arr.length ; i++){
+	        System.out.print(arr[i]+"\t");
+	    }
+	    System.out.println();
+	}
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while( t-- > 0){
+		    int n = sc.nextInt();
+		    int arr[] = new int[n];
+		    
+		    for( int i = 0 ; i<n ; i++){
+	            arr[i] = sc.nextInt();
+	        }
+		    
+		    int newarr[] = new int[n];
+		    convertArray(newarr, arr, n);
+		    printArray(newarr);
+		}
+	}
+
+    }
