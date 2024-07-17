a84bd33720c663c8b4690b8e2d01778ef1ab8381
Subroto <subroto.2003@gmail.com>
2024-07-17 23:18:41 +0530

    two sum optimal solution

--- LeetCode/1.dart
+++ LeetCode/1.dart
@@ -12,11 +12,26 @@ class Solution {
   }
 }
 
+// Optimal Method
+class Solution1 {
+  List<int> twoSum(List<int> nums, int target) {
+    Map<int, int> numMap = {};
+    for (int i = 0; i < nums.length; i++) {
+      int compliment = target - nums[i];
+      if (numMap.containsKey(compliment)) {
+        return [numMap[compliment]!, i];
+      }
+      numMap[nums[i]] = i;
+    }
+    throw Exception("No Solution Found");
+  }
+}
+
 void main() {
   // List<int> nums = [2, 5, 5, 11];
   // int target = 10;
-  List<int> nums = [3,2,3];
-  int target = 6;
+  List<int> nums = [14,15,89,100,56,21];
+  int target = 110;
   List<int> res = Solution().twoSum(nums, target);
   print(res);
 }
