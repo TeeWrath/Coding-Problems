94315f6a3bca2b9e2b36bfeabc3207585d9fe8c6
Subroto <subroto.2003@gmail.com>
2024-12-13 19:11:28 +0530

    pattern 16

---
+++ striver/pat16.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void printPattern(int n){
+    char ch = 'A';
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<= i; j++){
+            cout<< ch << " ";
+        }
+        cout << endl;
+        ch += 1;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    printPattern(n);
+    return 0;
+}
