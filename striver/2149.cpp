8baa80fa169d3fa8ce77d696f98c16d5330c93db
Subroto <subroto.2003@gmail.com>
2025-01-15 21:52:16 +0530

    lc 2149

---
+++ striver/2149.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Optimal
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans(n,0);
+
+        int posInd = 0; int negInd = 1;
+        for(int i=0; i<n; i++){
+            if(nums[i]>0){
+                ans[posInd] = nums[i];
+                posInd += 2;
+            }else{
+                ans[negInd] = nums[i];
+                negInd += 2;
+            }
+        }
+        return ans;
+    }
+};
