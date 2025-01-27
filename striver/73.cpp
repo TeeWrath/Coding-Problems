3a528fa955133993bd46101f659285360a3fc6a1
Subroto <subroto.2003@gmail.com>
2025-01-27 23:28:42 +0530

    73 lc better

--- striver/73.cpp
+++ striver/73.cpp
@@ -61,8 +61,37 @@ public:
     }
 };
 
+// Better approach
+class Solution2 {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> row(m,0);
+        vector<int> col(n,0);
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+}
+};
+
 int main(){
-    Solution sol = Solution();
+    Solution2 sol = Solution2();
     vector<vector<int>> matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
     sol.setZeroes(matrix);
     printMatrix(matrix);
