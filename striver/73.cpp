fc03b34bcd7615e84e40ec29b0543ad3873b9f5b
Subroto <subroto.2003@gmail.com>
2025-01-28 00:07:09 +0530

    73 lc optimal

--- striver/73.cpp
+++ striver/73.cpp
@@ -90,8 +90,55 @@ public:
 }
 };
 
+// Optimal Approach
+class Solution3 {
+    public:
+    void setZeroes(vector<vector<int>> & matrix){
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int col = 1;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+
+                    if(j != 0){
+                        matrix[0][j] = 0;
+                    }
+                    else{
+                        col = 0;
+                    }
+                }
+            }
+        }
+
+        for(int i =1 ;i<m ; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][j] != 0){
+                    if(matrix[0][j] == 0 || matrix[i][0] ==0){
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        if(matrix[0][0] == 0){
+            for(int j=0; j<n; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        if(col == 0){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
 int main(){
-    Solution2 sol = Solution2();
+    Solution3 sol = Solution3();
     vector<vector<int>> matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
     sol.setZeroes(matrix);
     printMatrix(matrix);
