53cbf9b95b6feb510f81be312c326bdbcb808a76
Subroto Banerjee <subroto.2003@gmail.com>
2025-02-12 15:37:38 +0530

    1021 lc using stack

--- striver/1021.cpp
+++ striver/1021.cpp
@@ -32,3 +32,25 @@ public:
                        // parentheses
     }
 };
+
+
+// Using Stack
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result;
+        stack<char> st;
+
+        for(char ch : s){
+            if(ch == '('){
+                if(!st.empty()) result += ch;
+                st.push(ch);
+            }else{
+                st.pop();
+                if(!st.empty()) result += ch;
+            }
+        }
+
+        return result;
+    }
+};
