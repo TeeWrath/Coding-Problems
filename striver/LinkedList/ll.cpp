66be8f317dd7fed0845f13815c26bcc4310bd31a
Subroto Banerjee <subroto.2003@gmail.com>
2025-02-15 13:49:06 +0530

    deletion

--- striver/LinkedList/ll.cpp
+++ striver/LinkedList/ll.cpp
@@ -53,18 +53,75 @@ bool checkIfPresent(Node* head, int num){
     return false;
 }
 
+// Deleting head of the Ll
+Node* delHead(Node* head){
+    if(head == NULL) return head;
+    Node* temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+}
+
+// Deleting tail of the Ll
+Node* delTail(Node* head){
+    if(head == NULL || head->next == NULL) return NULL;
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    delete temp->next;
+    temp->next = NULL;
+    return head;
+}
+
+// Delete kth element in Ll
+Node* delKEle(Node* head, int k){
+    if(head == NULL) return head;
+    if(k == 1){
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    int cnt = 1; Node* temp = head; Node* prev = NULL;
+    while(temp!=NULL){
+        if(cnt == k){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        cnt++;
+        prev = temp;
+        temp = temp->next;
+    }
+
+    return head;
+}
+
 int main(){
     vector<int> arr = {6,1,2,3,4,5};
     Node* head = convertArrToLl(arr);
 
     // Traversal of LL
-    // Node* temp = head;
-    // while(temp != nullptr){
-    //     cout << temp->data<< endl;
-    //     temp = temp->next;
-    // }
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data<< " ";
+        temp = temp->next;
+    }
+    cout << endl;
+
+    // head = delHead(head);
+    // head = delTail(head);
+    int k; cin >> k;
+    head = delKEle(head,k);
+    temp = head;
+    while(temp != nullptr){
+        cout << temp->data<< " ";
+        temp = temp->next;
+    }
+    cout << endl;
 
     // cout << lenOfLl(head);
-    cout << checkIfPresent(head, 0);
+    // cout << checkIfPresent(head, 0);
     return 0;
 }
