1529e8555bb8fa518bd2a61121c0848f6fdb3ad4
Subroto Banerjee <subroto.2003@gmail.com>
2025-02-21 23:48:17 +0530

    optimal 234

--- striver/LinkedList/234.cpp
+++ striver/LinkedList/234.cpp
@@ -31,4 +31,43 @@ bool isPalindrome(ListNode *head)
     return true;
 }
 
-// Optimal approach
+// Optimal approach - reversing half of list
+ListNode *reverseList(ListNode *head)
+{
+    if (head == NULL || head->next == NULL)
+        return head;
+    ListNode *newHead = reverseList(head->next);
+    ListNode *front = head->next;
+    front->next = head;
+    head->next = NULL;
+    return newHead;
+}
+bool isPalindrome(ListNode *head)
+{
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast->next != nullptr && fast->next->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    ListNode *newHead = reverseList(slow->next);
+
+    ListNode *first = head;
+    ListNode *second = newHead;
+
+    while (second != nullptr)
+    {
+        if (first->val != second->val)
+        {
+            reverseList(second);
+            return false;
+        }
+        first = first->next;
+        second = second->next;
+    }
+
+    reverseList(newHead);
+    return true;
+}
