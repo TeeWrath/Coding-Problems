124704283c85ceb9ee0661a10547a830f1680486
Subroto Banerjee <subroto.2003@gmail.com>
2025-03-09 11:30:50 +0530

    uodated solutions

--- LeetCode/contest-440/3477.cpp
+++ LeetCode/contest-440/3477.cpp
@@ -15,3 +15,28 @@ public:
         return check.size();
     }
 };
+
+// Better Solution - 1ms runtime
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int n = fruits.size();
+        vector<bool> visit(n,false);
+
+        int ans=0;
+        for(int i=0; i<n; ++i){
+            bool check = false;
+            for(int j=0; j<n; ++j){
+                if(!visit[j] && fruits[i] <= baskets[j]){
+                    visit[j] = true;
+                    check = true;
+                    break;
+                }
+            }
+            if(check == false){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
