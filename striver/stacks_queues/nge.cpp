7b13985e2f70e9c0874d0a3084c6bf9c5df5fcc2
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-02 17:20:25 +0530

    nge

---
+++ striver/stacks_queues/nge.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> nextGreaterElement(vector<int> nums) {
+    int n = nums.size();
+    vector<int> nge(n, -1);
+    stack<int> st;
+    int i = n - 1;
+    while (i >= 0) {
+        // Pop elements from stack while they are <= nums[i]
+        while (!st.empty() && st.top() <= nums[i]) {
+            st.pop();
+        }
+        // If stack is not empty, top is the next greater element
+        if (!st.empty()) {
+            nge[i] = st.top();
+        }
+        // Push current element
+        st.push(nums[i]);
+        i--;
+    }
+    return nge;
+}
+
+int main() {
+    vector<int> nums = {1, 3, 4, 2};
+    vector<int> ans = nextGreaterElement(nums);
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
