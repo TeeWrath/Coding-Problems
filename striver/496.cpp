51f38fd2ff21af332f710263f74f00b1a95f42c4
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-02 16:53:21 +0530

    496.cpp

---
+++ striver/496.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Most optimized solution
+class Solution {
+    public:
+        vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+            vector<int> ans(nums1.size(),-1);
+            for(int i=0; i< nums1.size(); i++){
+                int j=0;
+                while(nums2[j] != nums1[i])j++;
+                for(int k=j+1; k<nums2.size(); k++){
+                    if(nums2[k] > nums2[j]){
+                        ans[i] = nums2[k];
+                        k = nums2.size();
+                    }
+                }
+            }
+    
+            return ans;
+        }
+    };
