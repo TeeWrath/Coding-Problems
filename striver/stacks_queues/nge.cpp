9028ae1dbab5396380d56e43eb2678b643640892
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-02 17:27:11 +0530

    nge for circular array

--- striver/stacks_queues/nge.cpp
+++ striver/stacks_queues/nge.cpp
@@ -22,9 +22,29 @@ vector<int> nextGreaterElement(vector<int> nums) {
     return nge;
 }
 
+vector<int> nextGreaterElementCircular(vector<int> nums) {
+    int n = nums.size();
+    vector<int> nge(n, -1);
+    stack<int> st;
+    // int i = n - 1;
+    for(int i= 2*n-1; i>=0 ; i--) {
+        // Pop elements from stack while they are <= nums[i]
+        while (!st.empty() && st.top() <= nums[i%n]) {
+            st.pop();
+        }
+        // If stack is not empty, top is the next greater element
+        if (!st.empty()) {
+            nge[i%n] = st.top();
+        }
+        // Push current element
+        st.push(nums[i%n]);
+    }
+    return nge;
+}
+
 int main() {
     vector<int> nums = {1, 3, 4, 2};
-    vector<int> ans = nextGreaterElement(nums);
+    vector<int> ans = nextGreaterElementCircular(nums);
     for (int i = 0; i < ans.size(); i++) {
         cout << ans[i] << " ";
     }
