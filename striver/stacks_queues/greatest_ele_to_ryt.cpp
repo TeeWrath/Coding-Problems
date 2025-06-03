387f710f78791a8ff6ff7fa4feb07ede90f1af61
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-03 17:15:37 +0530

    something

--- striver/stacks_queues/greatest_ele_to_ryt.cpp
+++ striver/stacks_queues/greatest_ele_to_ryt.cpp
@@ -17,10 +17,39 @@ vector<int> ngeToRight(vector<int> &nums)
     return ans;
 }
 
+// using stack - greatest elements to the right
+vector<int> ngstackToRight(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> ans(n, 0);
+    stack<int> st; // stack to store indices
+    
+    // Iterate from right to left
+    for (int i = n - 1; i >= 0; i--)
+    {
+        // Pop elements from stack while they are smaller than current element
+        while (!st.empty() && nums[st.top()] <= nums[i])
+        {
+            st.pop();
+        }
+        
+        // If stack is not empty, count elements greater than current
+        if (!st.empty())
+        {
+            ans[i] = st.size();
+        }
+        
+        // Push current index to stack
+        st.push(i);
+    }
+    
+    return ans;
+}
+
 int main()
 {
     vector<int> nums = {1, 3, 4, 2};
-    vector<int> ans = ngeToRight(nums);
+    vector<int> ans = ngstackToRight(nums);
     for (int i = 0; i < ans.size(); i++)
     {
         cout << ans[i] << " ";
