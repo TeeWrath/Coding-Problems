7c779df409b2a82c31f1455d6f27ce76d0e7a4d1
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-03 21:23:00 +0530

    better

--- striver/stacks_queues/907.cpp
+++ striver/stacks_queues/907.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 // poor time complexity - O(n^2)
-int sumSubarrayMins(vector<int> &arr)
+int sumSubarrayMinsz(vector<int> &arr)
 {
     int n = arr.size();
     const int MOD = 1e9 + 7;
@@ -17,4 +17,71 @@ int sumSubarrayMins(vector<int> &arr)
         }
     }
     return res;
+}
+
+// TC = O(5N) = SC
+vector<int> nextSmallestElement(vector<int> & nums){
+    int n = nums.size();
+    vector<int> nse(n, n);
+    int i= n-1;
+    stack<int> st;
+
+    while(i>=0){
+        // Pop elements from stack while they are >= nums[i]
+        while (!st.empty() && nums[st.top()] >= nums[i]) {
+            st.pop();
+        }
+        // If stack is not empty, top is the next greater element
+        if (!st.empty()) {
+            nse[i] = st.top();
+        }
+        // Push current element
+        st.push(i);
+        i--;
+    }
+    return nse;
+}
+
+vector<int> prevSmallestElement(vector<int> & nums){
+    int n = nums.size();
+    vector<int> nse(n,-1);
+    int i= 0;
+    stack<int> st;
+
+    while(i<n){
+        // Pop elements from stack while they are >= nums[i]
+        while (!st.empty() && nums[st.top()] > nums[i]) {
+            st.pop();
+        }
+        // If stack is not empty, top is the next greater element
+        if (!st.empty()) {
+            nse[i] = st.top();
+        }
+        // Push current element
+        st.push(i);
+        i++;
+    }
+    return nse;
+}
+
+int sumSubarrayMins(vector<int>& arr) {
+    int n = arr.size();
+    const int MOD = 1e9 + 7;
+    vector<int> nse = nextSmallestElement(arr);
+    vector<int> pse = prevSmallestElement(arr);
+    int total =0;
+
+    for(int i=0; i<n; i++){
+        int left = i - pse[i];
+        int right = nse[i] - i;
+        total = (total + ((right * left * 1LL * arr[i]) %MOD)) % MOD;
+    }
+
+    return total;
+}
+
+int main(){
+    vector<int> arr = {11,81,94,43,3};
+    cout << "sum of subarray mins: " << sumSubarrayMins(arr) << endl;
+    return 0;
 }
