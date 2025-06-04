1e93143a836c24e09890b6ab9dd860328543a0bc
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-04 18:00:57 +0530

    optimal solution

--- striver/sliding_window_two_pointer/3.cpp
+++ striver/sliding_window_two_pointer/3.cpp
@@ -1,7 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int lengthOfLongestSubstring(string s){
+// TC - O(n^2), SC - O(256) size of hash array
+int lengthOfLongestSubstring1(string s){
     int lenMax = 0;
     for(int i =0; i<s.size(); i++){
         vector<int> hashArray(256,0);
@@ -15,8 +16,29 @@ int lengthOfLongestSubstring(string s){
     return lenMax;
 }
 
+// two pointer sliding window approach - TC: O(n) and SC: O(256) for the hash array
+int lengthOfLongestSubstring(string s){
+    int l=0, r=0;
+    vector<int> hashArray(256,-1);
+    int n = s.size();
+    int lenMax = 0;
+    while(r < n){
+        if(hashArray[s[r]] != -1){
+            if(hashArray[s[r]] >= l){
+                l = hashArray[s[r]] + 1;
+            }
+        }
+        int len = r-l +1;
+        lenMax = max(len,lenMax);
+        hashArray[s[r]] = r;
+        r++;
+    }
+
+    return lenMax;
+}
+
 int main(){
-    string s = "abcabcbb";
+    string s = "cadbzabcd";
     cout << lengthOfLongestSubstring(s) << endl;
     return 0;
 }
