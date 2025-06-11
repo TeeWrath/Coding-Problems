7ca6130b362ab0215a5a659454a54bca7654f8c2
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-11 21:44:14 +0530

    preorder traversal

---
+++ striver/binary_trees/traversals/preorder.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = nullptr;
+    }
+};
+
+void preOrder(Node* root, vector<int>& arr){
+    if(root == nullptr) return;
+
+    arr.push_back(root->data);
+
+    preOrder(root->left,arr);
+    preOrder(root->right,arr);
+}
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    vector<int> arr;
+
+    preOrder(root,arr);
+
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
