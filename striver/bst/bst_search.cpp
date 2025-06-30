bd081559803c114101fbd11eb2859f1d4057a3ab
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-30 15:36:20 +0530

    min and max valued ele in bst

--- striver/bst/bst_search.cpp
+++ striver/bst/bst_search.cpp
@@ -19,3 +19,16 @@ TreeNode* searchBST(TreeNode* root,int val){
     while(root && root->val!=val)root = val < root->val?root->left:root->right;
     return root;
 }
+
+// return min and max valued element of bst
+int returnMin(TreeNode* root){
+    if(!root)return -1;
+    if(root->left == NULL)return root->val;
+    returnMin(root->left);
+}
+
+int returnMax(TreeNode* root){
+    if(!root)return -1;
+    if(root->right == NULL)return root->val;
+    returnMax(root->right);
+}
