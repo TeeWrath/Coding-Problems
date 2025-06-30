cfc3e798cc79f9b78302028470570a250048b81a
Subroto Banerjee <subroto.2003@gmail.com>
2025-06-30 13:51:01 +0530

    symmetric tree

--- striver/binary_trees/101.cpp
+++ striver/binary_trees/101.cpp
@@ -14,6 +14,19 @@ struct TreeNode
     }
 };
 
-bool isSymmetric(TreeNode* root) {
-    
+bool isSymUtil(TreeNode *leftNode, TreeNode *rightNode)
+{
+    if (leftNode == NULL || rightNode == NULL)
+    {
+        return leftNode == rightNode;
+    }
+
+    return (leftNode->val == rightNode->val && isSymUtil(leftNode->left, rightNode->right) && isSymUtil(leftNode->right, rightNode->left));
+}
+bool isSymmetric(TreeNode *root)
+{
+    if (!root)
+        return true;
+
+    return isSymUtil(root->left, root->right);
 }
