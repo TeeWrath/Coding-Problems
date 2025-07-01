d86d5b5c758797872bf2fe3bd7e4033523f44623
Subroto Banerjee <subroto.2003@gmail.com>
2025-07-01 12:41:05 +0530

    template

---
+++ striver/bst/template.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int data)
+    {
+        val = data;
+        left = right = nullptr;
+    }
+};
+
+TreeNode* insertIntoBST(TreeNode* root, int val) {
+    if(!root) return new TreeNode(val);
+    TreeNode* cur = root;
+    while(true){
+        if(cur->val <= val){
+            if(cur->right != nullptr)cur = cur->right;
+            else{
+                cur->right = new TreeNode(val);
+                break;
+            }
+        }else{
+            if(cur->left != nullptr)cur = cur->left;
+            else{
+                cur->left = new TreeNode(val);
+                break;
+            }
+        }
+    }
+    return root;
+}
+
+TreeNode* createBST(){
+    int rt;
+    cout << "Enter root value: ";
+    cin >> rt;
+    TreeNode* root = new TreeNode(rt);
+    while(true){
+        cout << "Enter whole numeric value: ";
+        int value; cin >> value;
+        if(value == -1)break;
+        root = insertIntoBST(root,value);
+    }
+
+    return root;
+}
+
+void printBST(TreeNode* root){
+    if(!root)return;
+    cout << root->val << " ";
+    printBST(root->left);
+    printBST(root->right);
+}
+
+int main(){
+    TreeNode* root = createBST();
+    printBST(root);
+    return 0;
+}
