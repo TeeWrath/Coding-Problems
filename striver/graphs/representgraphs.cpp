e625c5a9fc18c0b51d369fba2f03178a7c9178bf
Subroto Banerjee <subroto.2003@gmail.com>
2025-07-03 09:32:23 +0530

    weighted graph

--- striver/graphs/representgraphs.cpp
+++ striver/graphs/representgraphs.cpp
@@ -28,6 +28,37 @@ vector<vector<int>> createDirectedGraph(int n, int m){
     return adjacent;
 }
 
+vector<vector<pair<int,int>>> createWeightedUndirectedGraph(int n,int m){
+    vector<vector<pair<int,int>>> adjacent(n+1);
+    cout << "Creating Undirected Weighted Graph" << endl;
+    for(int i=0; i<m; i++){
+        int u,v;
+        cout << "Enter node values for edges (u and v): ";
+        cin >> u >> v;
+        cout << "Enter weight between u and v: ";
+        int wt; cin >> wt;
+        adjacent[u].push_back({v,wt});
+        adjacent[v].push_back({u,wt});
+    }
+
+    return adjacent;
+}
+
+vector<vector<pair<int,int>>> createWeightedDirectedGraph(int n,int m){
+    vector<vector<pair<int,int>>> adjacent(n+1);
+    cout << "Creating Directed Weighted Graph" << endl;
+    for(int i=0; i<m; i++){
+        int u,v;
+        cout << "Enter node values for edges (from u to v): ";
+        cin >> u >> v;
+        cout << "Enter weight between u and v: ";
+        int wt; cin >> wt;
+        adjacent[u].push_back({v,wt});
+    }
+
+    return adjacent;
+}
+
 void printGraph(vector<vector<int>> graph){
     for(auto it: graph){
         for(int i=0; i< it.size(); i++){
@@ -37,10 +68,29 @@ void printGraph(vector<vector<int>> graph){
     }
 }
 
+void printWeightedGraph(vector<vector<pair<int,int>>> graph){
+    for(auto it: graph){
+        for(int i=0; i< it.size(); i++){
+            cout << "{"<< it[i].first <<"," << it[i].second << "}" << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     int n,m;
     cout << "Enter value of n and m respectively" << endl;
     cin >> n >> m;
+    vector<vector<int>> dg = createDirectedGraph(n,m);
+    vector<vector<int>> udg = createUndirectedGraph(n,m);
+    cout << "directed" << endl;
+    printGraph(dg);
+    cout << "undirected" << endl;
+    printGraph(udg);
+    return 0;
+}
+
+
     // graph is below using adjacency matrix - time costly
     // int adj[n+1][m+1];
     // for(int i=0; i<m; i++){
@@ -58,11 +108,3 @@ int main(){
     //     adjacent[u].push_back(v);
     //     adjacent[v].push_back(u); // for a directed graph, simply remove this line, rest is same
     // }
-    vector<vector<int>> dg = createDirectedGraph(n,m);
-    vector<vector<int>> udg = createUndirectedGraph(n,m);
-    cout << "directed" << endl;
-    printGraph(dg);
-    cout << "undirected" << endl;
-    printGraph(udg);
-    return 0;
-}
