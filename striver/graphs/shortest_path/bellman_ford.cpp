d377af5a657cc8ddfad6eb44f9171fb92337d63a
Subroto Banerjee <subroto.2003@gmail.com>
2025-07-12 11:56:15 +0530

    bellman ford

---
+++ striver/graphs/shortest_path/bellman_ford.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> bellmanFord(int V, vector<vector<int>> &edges, int src)
+    {
+        vector<int> distance(V, 1e8);
+        distance[src] = 0;
+        for (int i = 0; i < V - 1; i++)
+        {
+            for (auto it : edges)
+            {
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+                if (distance[u] != 1e8 && distance[u] + wt < distance[v])
+                {
+                    distance[v] = distance[u] + wt;
+                }
+            }
+        }
+        // to detec negative cycle
+        for (auto it : edges)
+        {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            if (distance[u] != 1e8 && distance[u] + wt < distance[v])
+                return {-1};
+        }
+
+        return distance;
+    }
+};
