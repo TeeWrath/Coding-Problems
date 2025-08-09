edb91b43687618afd438edce7a4fe0dbca23ed99
Subroto Banerjee <subroto.2003@gmail.com>
2025-08-09 11:45:25 +0530

    127

---
+++ striver/graphs/127.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+            queue<pair<string,int>> q;
+            q.push({beginWord,1});
+            set<string> st(wordList.begin(),wordList.end());
+            st.erase(beginWord);
+
+            while(!q.empty()){
+                auto [word, level] = q.front();
+                q.pop();
+
+                if(word == endWord)return level;
+
+                for(int i=0;i<word.size();i++){
+                    char originalWord = word[i];
+                    for(char ch = 'a';ch<= 'z';ch++){
+                        word[i] = ch;
+                        if(st.find(word) != st.end()){
+                            st.erase(word);
+                            q.push({word,level+1});
+                        }
+                    }
+                    word[i] = originalWord;
+                }
+            }
+
+            return 0;
+        }
+    };
