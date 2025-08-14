ba9d663d004e0f268e6d02c421bf4e73a1351bdf
Subroto Banerjee <subroto.2003@gmail.com>
2025-08-14 12:29:37 +0530

    print subse

--- striver/dynamic_programming/strings/1143.cpp
+++ striver/dynamic_programming/strings/1143.cpp
@@ -83,31 +83,88 @@ public:
 };
 
 // space optimized
-class Solution {
-    public:
-        int longestCommonSubsequence(string text1, string text2) {
-            int n1 = text1.size();
-            int n2 = text2.size();
-            vector<int> prev(n2+1,0);
-            vector<int> cur(n2+1,0);
+class Solutions
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n1 = text1.size();
+        int n2 = text2.size();
+        vector<int> prev(n2 + 1, 0);
+        vector<int> cur(n2 + 1, 0);
 
-            for(int ind1=1;ind1<=n1;ind1++){
-                for(int ind2=1;ind2<=n2;ind2++){
-                    if(text1[ind1-1]== text2[ind2-1])cur[ind2] = 1 + prev[ind2-1];
-                    else cur[ind2] = max(prev[ind2],cur[ind2-1]);
-                }
-                prev = cur;
+        for (int ind1 = 1; ind1 <= n1; ind1++)
+        {
+            for (int ind2 = 1; ind2 <= n2; ind2++)
+            {
+                if (text1[ind1 - 1] == text2[ind2 - 1])
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                else
+                    cur[ind2] = max(prev[ind2], cur[ind2 - 1]);
             }
-
-            return prev[n2];
+            prev = cur;
         }
-    };
+
+        return prev[n2];
+    }
+};
+
+// print subseq
+class Solution
+{
+public:
+    void longestCommonSubsequence(string text1, string text2)
+    {
+        int n1 = text1.size();
+        int n2 = text2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+
+        for (int ind1 = 1; ind1 <= n1; ind1++)
+        {
+            for (int ind2 = 1; ind2 <= n2; ind2++)
+            {
+                if (text1[ind1 - 1] == text2[ind2 - 1])
+                {
+                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                }
+                else
+                {
+                    dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                }
+            }
+        }
+
+        int len = dp[n1][n2];
+        string str(len, '$'); // Initialize with correct length
+        int i = n1, j = n2, ind = len - 1;
+
+        while (i > 0 && j > 0)
+        {
+            if (text1[i - 1] == text2[j - 1])
+            {
+                str[ind] = text1[i - 1];
+                ind--;
+                i--;
+                j--;
+            }
+            else if (dp[i - 1][j] > dp[i][j - 1])
+            {
+                i--;
+            }
+            else
+            {
+                j--;
+            }
+        }
+
+        cout << str << endl;
+    }
+};
 
 int main()
 {
     string text1, text2;
     cin >> text1 >> text2;
-
-    cout << Solution().longestCommonSubsequence(text1, text2) << endl;
+    Solution().longestCommonSubsequence(text1, text2);
     return 0;
 }
