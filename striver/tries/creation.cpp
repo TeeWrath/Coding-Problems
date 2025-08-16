6ca6899e1fb9f4d4c1d87b4ee2a90b39b1e47b12
Subroto Banerjee <subroto.2003@gmail.com>
2025-08-16 15:57:07 +0530

    208

---
+++ striver/tries/creation.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    Node *links[26];
+    bool flag = false;
+    bool containsKey(char ch)
+    {
+        return (links[ch - 'a'] != NULL);
+    }
+    void put(char ch, Node *node)
+    {
+        links[ch - 'a'] = node;
+    }
+
+    Node *get(char ch)
+    {
+        return links[ch - 'a'];
+    }
+
+    void setEnd()
+    {
+        flag = true; // Changed from false to true
+    }
+    bool isEnd()
+    {
+        return flag;
+    }
+};
+
+class Trie
+{
+private:
+    Node *root;
+
+public:
+    Trie()
+    {
+        root = new Node();
+    }
+
+    void insert(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]); // Added this line
+        }
+        node->setEnd();
+    }
+
+    bool search(string word)
+    {
+        Node *node = root;
+        for (int i = 0; i < word.size(); i++)
+        {
+            if (!node->containsKey(word[i]))
+            {
+                return false;
+            }
+            node = node->get(word[i]); // Added this line
+        }
+        return node->isEnd();
+    }
+
+    bool startsWith(string prefix)
+    {
+        Node *node = root;
+        for (int i = 0; i < prefix.size(); i++)
+        {
+            if (!node->containsKey(prefix[i]))
+            {
+                return false;
+            }
+            node = node->get(prefix[i]); // Added this line
+        }
+        return true;
+    }
+};
