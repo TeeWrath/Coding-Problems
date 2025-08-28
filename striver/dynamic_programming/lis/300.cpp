68b4871d2cb7c3d059a3b5c23568b6a19938b13f
Subroto Banerjee <subroto.2003@gmail.com>
2025-08-28 10:15:57 +0530

    tabulation and space optimized

--- striver/dynamic_programming/lis/300.cpp
+++ striver/dynamic_programming/lis/300.cpp
@@ -1,27 +1,86 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-    public:
-        int f(int i,int j, int n, vector<int>& nums,vector<vector<int>> &dp){
-            if(i==n)return 0;
-            if(dp[i][j+1] != -1)return dp[i][j+1];
-            int pick=0;
-            int notpick = f(i+1,j,n,nums,dp);
-            if(j==-1 || nums[i]>nums[i-1])pick = 1+ f(i+1,i,n,nums,dp);
+class Solutionm
+{
+public:
+    int f(int i, int j, int n, vector<int> &nums, vector<vector<int>> &dp)
+    {
+        if (i == n)
+            return 0;
+        if (dp[i][j + 1] != -1)
+            return dp[i][j + 1];
+        int pick = 0;
+        int notpick = f(i + 1, j, n, nums, dp);
+        if (j == -1 || nums[i] > nums[i - 1])
+            pick = 1 + f(i + 1, i, n, nums, dp);
 
-            return dp[i][j+1] = max(pick,notpick);
-        }
-    public:
-        int lengthOfLIS(vector<int>& nums) {
-            int n = nums.size();
-            vector<vector<int>> dp(n,vector<int>(n+1,-1));
-            return f(0,-1,n,nums,dp);
-        }
-    };
+        return dp[i][j + 1] = max(pick, notpick);
+    }
 
-int main(){
-    vector<int> nums = {10,9,2,5,3,7,101,18};
-    cout << Solution().lengthOfLIS(nums)<< endl;
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        return f(0, -1, n, nums, dp);
+    }
+};
+
+class Solutiont
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n + 1, 0));
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i - 1; j >= -1; j--)
+            {
+                int pick = 0;
+                int notpick = dp[i + 1][j+1];
+                if (j == -1 || nums[i] > nums[j])
+                    pick = 1 + dp[i + 1][i+1];
+
+                dp[i][j + 1] = max(pick, notpick);
+            }
+        }
+
+        return dp[0][0];
+    }
+};
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> after(n + 1, 0),cur(n+1,0);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = i - 1; j >= -1; j--)
+            {
+                int pick = 0;
+                int notpick = after[j+1];
+                if (j == -1 || nums[i] > nums[j])
+                    pick = 1 + after[i+1];
+
+                cur[j + 1] = max(pick, notpick);
+            }
+            after = cur;
+        }
+
+        return cur[0];
+    }
+};
+
+int main()
+{
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    cout << Solution().lengthOfLIS(nums) << endl;
     return 0;
 }
