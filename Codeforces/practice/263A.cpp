f2ee899d980f3994d17aab2bd1c4af0f63c41792
Subroto Banerjee <subroto.2003@gmail.com>
2025-09-29 12:51:32 +0530

    263a

--- Codeforces/practice/263A.cpp
+++ Codeforces/practice/263A.cpp
@@ -48,7 +48,15 @@ vi g[N];
 int a[N];
 
 void solve() {
-  int i, j, n, m;
+  int i, j, n=0;
+  for( i=1;i<=5;i++){
+    for(j=1;j<=5;j++){
+        cin >> n;
+        if(n==1){
+            cout << abs(i-3) + abs(j-3)<< endl;
+        }
+    }
+  }
 }
 
 int main() {
