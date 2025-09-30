0cd4f51249987e949f1079e570200b2ba53d52d6
Subroto Banerjee <subroto.2003@gmail.com>
2025-09-30 20:13:11 +0530

    twosum

---
+++ revision/1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            int n = nums.size();
+            map<int,int> mpp;
+            for(int i=0;i<n;i++){
+                if(mpp.find(target - nums[i]) == mpp.end())mpp[nums[i]]=i;
+                else return {mpp[target-nums[i]],i};
+            }
+
+            return {-1,-1};
+        }
+    };
