e4aa4109017376950bb6896e590e156d2c554d2d
Subroto Banerjee <subroto.2003@gmail.com>
2025-10-03 16:58:14 +0530

    permutw

--- revision/printallpermutations.cpp
+++ revision/printallpermutations.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution
+class Solutionm
 {
 public:
     void f(vector<int> &ds,vector<vector<int>> & ans, vector<int> &mpp, vector<int> &nums){
@@ -32,8 +32,44 @@ public:
     }
 };
 
+class Solution
+{
+    public:
+        void f(int ind, vector<int> &nums, vector<vector<int>> &ans){
+            if(ind == nums.size()){
+                ans.push_back(nums);
+                return;
+            }
+
+            for(int i=ind;i<nums.size();i++){
+                swap(nums[ind],nums[i]);
+                f(ind+1,nums,ans);
+                swap(nums[ind],nums[i]);
+            }
+        }
+public:
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        f(0,nums,ans);
+        return ans;
+    }
+};
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int n; cin >> n;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int ele; cin >> ele;
+        arr.push_back(ele);
+    }
+    vector<vector<int>> ans = Solution().permute(arr);
+    for(int i=0;i<ans.size();i++){
+        for(auto it: ans[i]){
+            cout << it << ' ';
+        }
+        cout << '\n';
+    }
     return 0;
 }
