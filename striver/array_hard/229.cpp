85cc43304b9b998665b6d79ac48b2229a0020189
Subroto <subroto.2003@gmail.com>
2025-10-14 13:15:48 +0530

    majority

--- striver/array_hard/229.cpp
+++ striver/array_hard/229.cpp
@@ -42,6 +42,50 @@ public:
     }
 };
 
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+       int n = nums.size();
+       int check = n/3;
+       int cnt1=0,cnt2=0;
+       int ele1=0,ele2=0;
+       vi res;
+
+       fo(i,n){
+        if(cnt1==0 && nums[i]!=ele2){
+            ele1=nums[i];
+            cnt1=1;
+        }
+        else if(cnt2==0 && nums[i]!=ele1){
+            ele2= nums[i];
+            cnt2=1;
+        }
+        else if(nums[i] ==ele1)cnt1++;
+        else if(nums[i] == ele2)cnt2++;
+        else{
+            cnt1--;
+            cnt2--;
+        }
+       }
+
+       cnt1=0,cnt2=0;;
+       fo(i,n){
+        if(nums[i] ==ele1)cnt1++;
+        else if(nums[i] ==ele2)cnt2++;
+        if(cnt1 == check){
+            res.pb(ele1);
+            break;
+        }
+        if(cnt2 == check){
+            res.pb(ele2);
+            break;
+        }
+       }
+
+       return res;
+    }
+};
+
 int main(int argc, char const *argv[])
 {
     vi nums;
@@ -52,7 +96,7 @@ int main(int argc, char const *argv[])
         nums.pb(ele);
     }
 
-    vi ans = Solutionb().majorityElement(nums);
+    vi ans = Solution().majorityElement(nums);
     for(auto it: ans){
         cout << it<<" ";
     }
