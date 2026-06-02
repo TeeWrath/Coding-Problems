3307778ea83975ad5c14ec4e2de15e5a431c5a51
TeeWrath <subroto.2003@gmail.com>
2026-06-02 06:42:25 +0530

    feat: more patterns

--- revision/pattern/p1.cpp
+++ revision/pattern/p1.cpp
@@ -144,10 +144,94 @@ void pattern10(int n){
     }
 }
 
+void pattern11(int n){
+    bool row=true;
+    for(int i=1;i<=n;i++){
+        cout<<row;
+        bool dig= !row;
+        for(int j=2;j<=i;j++){
+            cout<<dig;
+            dig = !dig;
+        }
+        cout << endl;
+        row = !row;
+    }
+}
+
+void pattern12(int n){
+    for(int i=1;i<=n;i++){
+        int spaces = 2*(n-1);
+        for(int j=1;j<=i;j++)cout <<j;
+        for(int j=1;j<= spaces;j++)cout << " ";
+        for(int j=i; j>=1;j--)cout<<j;
+        cout << endl;
+        spaces -=2;
+    }
+}
+
+void pattern13(int n){
+    int num=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<num<<" ";
+            num++;
+        }
+        cout << endl;
+    }
+}
+
+void pattern14(int n){
+    for(int i=1;i<=n;i++){
+        char var = 'A';
+        for(int j=1;j<=i;j++){
+            cout<<var<<" ";
+            var++;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern15(int n){
+    for(int i=n;i>=1;i--){
+        char var = 'A';
+        for(int j=1;j<=i;j++){
+            cout<<var<<" ";
+            var++;
+        }
+        cout<<endl;
+    }
+}
+
+void pattern16(int n){
+    char var = 'A';
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<var<<" ";
+        }
+        var++;
+        cout<<endl;
+    }
+}
+
+// may repeat
+void pattern17(int n){
+    for(int i=0;i<n;i++){
+        char var = 'A';
+        for(int j=0;j<= n -i - 1;j++)cout << " ";
+        int bp = ((2*i)+1)/2;
+        for(int j=1;j<=(2*i)+1;j++){
+            cout << var;
+            if(j <= bp) var++;
+            else var--;
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
-    pattern10(n);
+    pattern17(n);
     return 0;
 }
