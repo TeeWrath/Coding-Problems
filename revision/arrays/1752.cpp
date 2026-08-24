40b1d4bced4c4f77855c094eac76fbb6e4b84f88
TeeWrath <subroto.2003@gmail.com>
2026-08-24 12:45:32 +0530

    some practice

---
+++ revision/arrays/1752.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n = nums.size();
+        int cnt=0;
+        for(int i=0;i<n-1;i++){
+            if(nums[i] > nums[i+1]){
+                cnt++;
+                if(cnt >1)return false;
+            }
+        }
+        if(cnt == 1 && (nums[0] < nums[n-1]))return false;
+        return true;
+    }
+};
