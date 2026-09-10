8640babe889b109f270ce0a322598b2cdcdc7990
TeeWrath <subroto.2003@gmail.com>
2026-09-10 08:43:42 +0530

    longestseq

---
+++ revision/arrays/longest_cons_seq.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestConsecutiveSubsequence(vector<int> & nums){
+    int n = nums.size();
+    if(n==0)return 0;
+
+    int longest = 1;
+    unordered_set<int> st;
+
+    for(int i=0;i<n;i++){
+        st.insert(nums[i]);
+    }
+
+    for(auto it: st){
+        if(st.find(it-1) == st.end()){
+            int cnt =1;
+            int x = it;
+            while(st.find(x+1) != st.end()){
+                cnt++;
+                x++;
+            }
+            longest = max(cnt,longest);
+        }
+    }
+
+    return longest;
+}
+
+int main(){
+    // vector<int> nums = {10,22,12,3,0,6};
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    cout << longestConsecutiveSubsequence(nums) << endl;
+    return 0;
+}
